fa33058d633f44b69077c932f7171ca1cc424c17
Ramos Keke <ramos.keke@example.com>
2025-10-24 22:00:10 +0000

    modification du backend avis

--- backend/src/reviews/reviews.module.ts
+++ backend/src/reviews/reviews.module.ts
@@ -4,11 +4,24 @@ import { Module } from '@nestjs/common';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '7d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [ReviewsController],
   providers: [ReviewsService, PrismaService],
-  exports: [ReviewsService], // Exporter pour utilisation dans d'autres modules
+  exports: [ReviewsService],
 })
 export class ReviewsModule {}
